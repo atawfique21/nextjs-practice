bb418e95eb499f7250f0525126ef1d18362e7515
A Tawfique <easternselling@gmail.com>
2021-06-27 20:41:59 -0400

    Further checking revalidate.

--- pages/index.js
+++ pages/index.js
@@ -2,24 +2,28 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import {getList} from '../lib/users'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
+  const list = await getList()
   return {
     props: {
       allPostsData,
-    }
+      list
+    },
+    revalidate: 10
   }
 }
 
-export default function Home({allPostsData}) {
+export default function Home({allPostsData, list}) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        <p>{list}</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
